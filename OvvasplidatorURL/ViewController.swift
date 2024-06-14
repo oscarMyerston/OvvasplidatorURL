975f9257dae8e1700271b07ab738e228bce03a1c
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2024-06-13 21:57:18 -0500

    First Commit

---
+++ OvvasplidatorURL/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  OvvasplidatorURL
+//
+//  Created by Oscar David Myerston Vega on 13/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
